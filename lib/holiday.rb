c69bd2cb9b18473eb022ef5a44e104fabcaba67f
Chance Smith <chancesmith97@hotmail.com>
2019-02-25 02:57:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,6 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       puts "  #{holiday_name.to_s.split("_").colllect{|split_name| split_name.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+    
 
 
     end
